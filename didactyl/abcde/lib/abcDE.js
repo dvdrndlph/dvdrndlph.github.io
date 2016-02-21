6028e3e94e4e158857e956b57c782307d2c165b8
David Randolph <rndlph@motorola.com>
2016-02-20 22:16:13 -0600

    Number measures./util/deploy Number

--- didactyl/abcde/lib/abcDE.js
+++ didactyl/abcde/lib/abcDE.js
@@ -527,7 +527,7 @@ function AbcDE() {
         set_radio('restore', localStorage.getItem('restore'));
         set_radio('keypad', localStorage.getItem('keypad'));
         var numbering_setting = localStorage.getItem('measure_number_interval');
-        if (numbering_setting === undefined && numbering_setting === '') {
+        if (numbering_setting === '') {
             numbering_setting = DEFAULT_NUMBERING_INTERVAL;
         }
         set_field('measure_number_interval', numbering_setting);
