32ac3079abef78008e75c9151bc27fc8f720b36f
David Randolph <dave@tuningbell.com>
2018-12-20 15:09:15 -0600

    Tweaks

--- didactyl/abcde/lib/abcDE_full.js
+++ didactyl/abcde/lib/abcDE_full.js
@@ -1,4 +1,4 @@
-/* abcDE_full.js v6.0.27 */
+/* abcDE_full.js v6.0.28 */
 !function(a, b) {
  "object" == typeof module && "object" == typeof module.exports ? module.exports = a.document ? b(a, !0) : function(a) {
   if (!a.document) throw new Error("jQuery requires a window with a document");
--- didactyl/abcde/lib/abcDE_min.js
+++ didactyl/abcde/lib/abcDE_min.js
@@ -1,4 +1,4 @@
-/* abcDE_min.js v6.0.27 */
+/* abcDE_min.js v6.0.28 */
 /** @license
  *
  * Copyright (c) 2015, 2016 David A. Randolph.
